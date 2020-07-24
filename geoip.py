689aa4fbaab7d155ff45d320ea01ea609651d40c
JuliaBielsa <42615040+JuliaBielsa@users.noreply.github.com>
2020-07-24 13:17:17 +0200

    Update geoip.py

--- geoip.py
+++ geoip.py
@@ -6,18 +6,36 @@ import os
 ipList = [line.rstrip('\n') for line in open('iplist.txt')]
 g = open('output.txt','w')
 
-for ip in ipList:
 
-    r = urllib.request.urlopen("http://ip-api.com/xml/"+ip).read()
-    with open('output.xml', 'w') as f:
-        f.write(r.decode('utf8'))
+def localizarIP():
+    location = input("Enter the type of location you want (country, region or city): ")
 
-    root = ET.parse("output.xml").getroot()
+    for ip in ipList:
 
-    for tag in root.iter('country'):
-        country=(tag.text)
+        r = urllib.request.urlopen("http://ip-api.com/xml/"+ip).read()
+        with open('output.xml', 'w') as f:
+            f.write(r.decode('utf8'))
 
-    g.write(ip+" "+country)
-    g.write("\n")
+        root = ET.parse("output.xml").getroot()
+        if(location=="region"):
+            for tag in root.iter("regionName"):
+                country=(tag.text)
+        else:
+            for tag in root.iter(location):
+                country=(tag.text)
+
+        g.write(ip+" "+country)
+        g.write("\n")
+
+for i in range(0,10000):
+    try:
+        localizarIP()
+    except:
+        print("Upss looks like you didn't write it correctly, try again!\n")
+        continue
+    break
+
+
+
 
 os.remove("output.xml")
